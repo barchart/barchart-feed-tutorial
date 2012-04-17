e201169351d131acba6466dcb256c505e9a82bd6
Gavin Litchfield <gavinm.litchfield@gmail.com>
2012-04-17 10:32:04 -0500

    Remake after git destroyed all the examples

---
+++ src/main/java/com/barchart/feed/ddf/datalink/DatalinkExample1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.barchart.feed.ddf.datalink;
+
+/**
+ * @author g-litchfield
+ *
+ */
+public class DatalinkExample1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
